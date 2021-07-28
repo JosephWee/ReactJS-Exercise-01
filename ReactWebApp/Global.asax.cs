50d20826d2bee0d091e57a463e42d7461f2fe20f
JosephWee <joseph.wee.tze.yeong@gmail.com>
2021-07-28 10:01:37 +0800

    Commit-2021-07-28-1001 Attempt to Transform JSX files to JS files (with equivalent React.createElement calls) was unsuccessful For more information please see: https://reactjs.net/tutorials/aspnet4.html https://babeljs.io/setup#installation https://docs.microsoft.com/en-us/visualstudio/modeling/writing-a-t4-text-template?view=vs-2019 https://github.com/Taritsyn/JavaScriptEngineSwitcher/wiki/Registration-of-JS-engines

---
+++ ReactWebApp/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using JavaScriptEngineSwitcher.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ReactWebApp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        /// <summary>
+        /// Attempt to Transform JSX files to JS files (with equivalent React.createElement calls) was unsuccessful
+        /// For more information please see:
+        /// https://reactjs.net/tutorials/aspnet4.html
+        /// https://babeljs.io/setup#installation
+        /// https://docs.microsoft.com/en-us/visualstudio/modeling/writing-a-t4-text-template?view=vs-2019
+        /// https://github.com/Taritsyn/JavaScriptEngineSwitcher/wiki/Registration-of-JS-engines
+        /// </summary>
+        protected void Application_Start()
+        {
+            //AreaRegistration.RegisterAllAreas();
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
+            JsEngineSwitcherConfig.Configure(JsEngineSwitcher.Current);
+        }
+    }
+}
---
+++ ReactWebApp/JSX/SimpleApp.jsx
@@ -0,0 +1,10 @@
+﻿class SimpleApp extends React.Component {
+    render() {
+        return <h1>Hello, World</h1>;
+    }
+}
+
+ReactDOM.render(
+    <SimpleApp />,
+    document.getElementById('root')
+);
